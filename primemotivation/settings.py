235f2803979ee95569f0089bcedb77483e701426
samuel <chyken07@gmail.com>
2023-03-13 12:19:28 -0700

    static config

--- primemotivation/settings.py
+++ primemotivation/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'primemotivation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'QJ3vEI19bH4ZkZSN5igj',
+        'HOST': 'containers-us-west-122.railway.app',
+        'PORT': '7370',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -123,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'mainapp/static'),
 ]
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
